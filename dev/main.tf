ec0b69ec4bd6db9d8e01b704816ebf2a4c65f693
Levi McDonough <levimcdonough@gmail.com>
2025-04-20 17:03:34 -0400

    adds terraform configuration with local backend

---
+++ dev/main.tf
@@ -0,0 +1,18 @@
+# terraform block with local backend
+terraform {
+
+  # terraform version
+  required_version = ">= 1.11.0"
+
+  # required providers config
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.95.0"
+    }
+  }
+  # backend config
+  backend "local" {
+    path = "terraform.tfstate"
+  }
+}
