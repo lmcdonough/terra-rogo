a0e5552c8686b20a5e82d7dfe060eaa087b3feac
Levi McDonough <levimcdonough@gmail.com>
2025-04-26 17:11:49 -0400

    refactor: switch AWS credentials to use environment variables and remove unused variables

--- dev/variables.tf
+++ dev/variables.tf
@@ -1,15 +1,1 @@
-variable "aws_access_key" {
-  description = "the aws access key"
-  type        = string
-}
 
-variable "aws_secret_key" {
-  description = "the aws secret key"
-  type        = string
-}
-
-variable "aws_region" {
-  description = "the aws region"
-  type        = string
-  default     = "us-east-1"
-}
