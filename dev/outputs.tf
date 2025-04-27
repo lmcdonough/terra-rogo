7413125e5a27ee5dee957685cc3495827ab74e42
Levi McDonough <levimcdonough@gmail.com>
2025-04-27 16:04:16 -0400

    fix: update output to reference ALB public DNS instead of EC2 instance

--- dev/outputs.tf
+++ dev/outputs.tf
@@ -1,4 +1,4 @@
-output "aws_instance_public_dns" {
-  value       = "http://${aws_instance.nginx1.public_dns}"
-  description = "the public dns name of the nginx server on the ec2 instance"
+output "aws_alb_public_dns" {
+  value       = "http://${aws_lb.nginx.dns_name}"
+  description = "The public DNS of the ALB"
 }
