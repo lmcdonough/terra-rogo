b57cd2fbb8f1dcca86bdc10af3030fe30a13d769
Levi McDonough <levimcdonough@gmail.com>
2025-04-27 15:49:06 -0400

    fix: correct load_balancer_arn reference in aws_lb_listener resource

--- dev/loadbalancer.tf
+++ dev/loadbalancer.tf
@@ -20,25 +20,25 @@ resource "aws_lb_target_group" "nginx" {
 
 # aws_lb_listener
 resource "aws_lb_listener" "nginx" {
-  load_balancer_arn = aws_lg.nginx.arn
+  load_balancer_arn = aws_lb.nginx.arn # ARN of the load balancer
   port              = "80"
   protocol          = "HTTP"
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.nginx.arn
+    target_group_arn = aws_lb_target_group.nginx.arn # ARN of the target group
   }
   tags = local.common_tags
 }
 
 # aws_lb_target_group_attachment
 resource "aws_lb_target_group_attachment" "nginx1" {
-  target_group_arn = aws_lb_target_group.nginx.arn
-  target_id        = aws_instance.nginx1.id
+  target_group_arn = aws_lb_target_group.nginx.arn # ARN of the target group
+  target_id        = aws_instance.nginx1.id        # ID of the instance to be attached
   port             = 80
 }
 
 resource "aws_lb_target_group_attachment" "nginx2" {
-  target_group_arn = aws_lb_target_group.nginx.arn
-  target_id        = aws_instance.nginx2.id
+  target_group_arn = aws_lb_target_group.nginx.arn # ARN of the target group
+  target_id        = aws_instance.nginx2.id        # ID of the instance to be attached
   port             = 80
 }
